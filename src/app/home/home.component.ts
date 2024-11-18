c899456a8aa0491b97b448e11b92571318d93e8f
esaith <hounds2003@hotmail.com>
2024-11-17 21:59:12 -0500

    Manually created scrolling effect. Created nav and couple basic template pages.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,53 @@
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { from, fromEvent } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('scrollbackground') scrollbackground?: ElementRef;
+  @ViewChildren('section') sections?: QueryList<ElementRef>;
+  private html?: HTMLElement;
+  private maxScrollHeight = 0;
+  private maxScrollBuffer = 400;
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
+
+  ngAfterViewInit() {
+    if (this.sections) {
+      for (const section of this.sections) {
+        this.maxScrollHeight += section.nativeElement.clientHeight;
+      }
+    }
+
+    this.maxScrollHeight += this.maxScrollBuffer;
+
+    if (this.scrollbackground) {
+      const height = this.maxScrollHeight.toString() + 'px';
+      console.log(height);
+      this.renderer.setStyle(this.scrollbackground.nativeElement, 'height', height);
+    }
+
+    fromEvent(window, 'scroll').subscribe((x) => {
+      if (this.sections) {
+        let rollingSumOfClientHeights = 0;
+
+        for (let i = 0; i < this.sections.length; ++i) {
+          const section = this.sections.get(i)?.nativeElement;
+
+          if (window.scrollY > rollingSumOfClientHeights + this.maxScrollBuffer || i === this.sections?.length - 1 && window.scrollY > rollingSumOfClientHeights) {
+            const diff = Math.abs(rollingSumOfClientHeights - window.scrollY);
+            console.log(rollingSumOfClientHeights, window.scrollY, diff);
+            this.renderer.setStyle(section, 'transform', `translateY(-${diff / section.clientHeight * 100 * 1.24}vh)`);
+          } else {
+            this.renderer.setStyle(section, 'transform', `translateY(0vh)`);
+          }
+
+          rollingSumOfClientHeights += section.clientHeight;
+        }
+      }
+    });
+  }
+}
